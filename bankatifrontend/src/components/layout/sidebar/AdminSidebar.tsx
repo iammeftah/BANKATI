8356ed3386138607ad88f816da5da1699f4a254c
itserror808 <meftahahmedreda02@gmail.com>
2025-01-08 17:18:43 +0100

    Done

--- bankatifrontend/src/components/layout/sidebar/AdminSidebar.tsx
+++ bankatifrontend/src/components/layout/sidebar/AdminSidebar.tsx
@@ -1,6 +1,5 @@
-// src/components/sidebar/AdminSidebar.tsx
 import React from 'react';
-import {Link, useLocation, useNavigate} from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import { AgentSpaceMenu } from './menu/AgentSpaceMenu';
 import { ClientSpaceMenu } from './menu/ClientSpaceMenu';
 import { HistoryMenu } from './menu/HistoryMenu';
@@ -10,17 +9,17 @@ export const AdminSidebar: React.FC = () => {
     const location = useLocation();
 
     return (
-        <div className="w-64 bg-gray-800 min-h-screen text-white">
+        <div className="w-64 bg-gray-900 min-h-screen text-gray-300">
             <div className="p-4">
-                <h2 className="text-xl font-bold mb-4">Backoffice</h2>
-                <div className="space-y-6">
+                <h2 className="text-2xl font-bold mb-6 text-white">Backoffice</h2>
+                <div className="space-y-8">
                     <AgentSpaceMenu currentPath={location.pathname} />
                     <ClientSpaceMenu currentPath={location.pathname} />
                     <HistoryMenu currentPath={location.pathname} />
                     <Password currentPath={location.pathname} />
-
                 </div>
             </div>
         </div>
     );
 };
+
